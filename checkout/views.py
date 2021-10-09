a0576d7656da1f85f2bdd6a55c06ebeb2f7d3ddd
andrewskyboss <andrew.cybossky@gmail.com>
2021-10-09 16:15:11 +0000

    update

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
+# from django.views.decorators.http import require_POST
+from django.contrib import messages
+# from django.conf import settings
+
+from .forms import OrderForm
+# from .models import Order, OrderLineItem
 
 # Create your views here.
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request,
+                           "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
+    order_form = OrderForm(form_data)
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
