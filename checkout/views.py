7593c2cbd898e82cadf5dbefef5c4666a0fd0a5b
andrewskyboss <andrew.cybossky@gmail.com>
2021-10-09 23:51:47 +0000

    update

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,9 @@ import json
 # Create your views here.
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request,
@@ -24,13 +27,27 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    # print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, ('Stripe public key is missing. '
+                                   'Did you forget to set it in '
+                                   'your environment?'))
+
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JimkbJBRuP8yXrnGPq5EkY9Y3Jyb1cy6DDO4R91gbzkRMlsdQz6OWRz6becX3PigydoWXaFAww46bq7uJ7suTlD00tWtV1rqS',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
